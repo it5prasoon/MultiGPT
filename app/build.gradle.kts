f9748ad04014d598c240af6a3da12e562fe5b7ee
it5prasoon <prasoonk187@gmail.com>
2025-12-07 10:03:15 +0530

    Bumped up version 1.25.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.matrix.multigpt"
         minSdk = 31
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.25.1"
+        versionCode = 5
+        versionName = "1.25.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
